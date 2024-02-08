21ef2b208a056c2006bcab8b403ce6f8d4ada5e9
Vladimir <68393739+Theend12345@users.noreply.github.com>
2024-02-08 21:24:28 +0300

    progect create

---
+++ app/src/main/java/kjxv/dietmy/com/MainActivity.kt
@@ -0,0 +1,11 @@
+package kjxv.dietmy.com
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
